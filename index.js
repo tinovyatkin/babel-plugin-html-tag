cb7619b216390aec32e3234d8c2bb51ab2ebb8a9
Tino Vyatkin <tino@vtkn.io>
2019-04-02 13:08:13 -0400

    lint

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const placeholder = '_BABEL_HTML_TAG_';
 const placeholderRx = /_babel_html_tag_/gi;
 
 /**
- * 
- * @param {string} name 
- * @param {string | string[]} [option=html] 
+ *
+ * @param {string} name
+ * @param {string | string[]} [option=html]
  * @returns {string[]}
  */
 function getNames(name, option = ['html']) {
@@ -31,8 +31,8 @@ function getNames(name, option = ['html']) {
 }
 
 /**
- * 
- * @param {string} text 
+ *
+ * @param {string} text
  * @returns {string}
  */
 function escapeStaticString(text) {
@@ -40,81 +40,81 @@ function escapeStaticString(text) {
   return text.replace(/(?<!\\)'/g, "\\'").replace(/\n+/g, '\\n');
 }
 
-
-module.exports = 
-/**
- * @param {import('@babel/core')} babel
- * @returns {{ visitor: Visitor }}
- */
-babel => {
-  const t = babel.types;
-
+module.exports =
   /**
-   * 
-   * @param {NodePath} template 
-   * @param {Pick<htmlMinifier.Options, Exclude<keyof htmlMinifier.Options, ['quoteCharacter', 'sortAttributes', 'removeAttributeQuotes', 'sortClassName', 'removeTagWhitespace']>>} [options={}]
+   * @param {import('@babel/core')} babel
+   * @returns {{ visitor: Visitor }}
    */
-  function minify(template, options = {}) {
-    /** @type {{ node: TemplateLiteral }} */
-    const { node } = template.get('quasi');
-    const { quasis } = node;
-    const cookedQuasis = quasis.map(quasi => quasi.value.cooked);
+  babel => {
+    const t = babel.types;
 
-    let minifyCSS = true;
-    for(const part of cookedQuasis) {
-      const openingStyles = (part.match(/<style>/gim) || []).length;
-      const closingStyles = (part.match(/<\/style>/gim) || []).length;
-      if(openingStyles > closingStyles) {
-        minifyCSS = false; break;
+    /**
+     *
+     * @param {NodePath} template
+     * @param {Pick<htmlMinifier.Options, Exclude<keyof htmlMinifier.Options, ['quoteCharacter', 'sortAttributes', 'removeAttributeQuotes', 'sortClassName', 'removeTagWhitespace']>>} [options={}]
+     */
+    function minify(template, options = {}) {
+      /** @type {{ node: TemplateLiteral }} */
+      const { node } = template.get('quasi');
+      const { quasis } = node;
+      const cookedQuasis = quasis.map(quasi => quasi.value.cooked);
+
+      let minifyCSS = true;
+      for (const part of cookedQuasis) {
+        const openingStyles = (part.match(/<style>/gim) || []).length;
+        const closingStyles = (part.match(/<\/style>/gim) || []).length;
+        if (openingStyles > closingStyles) {
+          minifyCSS = false;
+          break;
+        }
+      }
+      const html = cookedQuasis.join(placeholder);
+      const minified = htmlMinifier.minify(html, {
+        collapseWhitespace: true,
+        conservativeCollapse: false,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        preserveLineBreaks: false,
+        ...options,
+        minifyCSS,
+        /* setting below must be enforced in order to prevent replacement mistakes */
+        quoteCharacter: '"',
+        sortAttributes: false,
+        removeAttributeQuotes: false,
+        sortClassName: false,
+        removeTagWhitespace: false,
+      });
+
+      const parts = minified.split(placeholderRx);
+      if (parts.length > 1) {
+        parts.forEach((value, i) => {
+          template
+            .get('quasi')
+            .get('quasis')
+            [i].replaceWith(
+              t.templateElement(
+                { cooked: value, raw: value },
+                i === parts.length - 1,
+              ),
+            );
+        });
+        template.replaceWith(template.get('quasi'));
+      } else {
+        template.replaceWithSourceString(`'${escapeStaticString(minified)}'`);
       }
     }
-    const html = cookedQuasis.join(placeholder);
-    const minified = htmlMinifier.minify(html, {
-      collapseWhitespace: true,
-      conservativeCollapse: false,
-      removeComments: true,
-      removeEmptyAttributes: true,
-      preserveLineBreaks: false,
-      ...options,
-      minifyCSS,
-      /* setting below must be enforced in order to prevent replacement mistakes */
-      quoteCharacter: '"',
-      sortAttributes: false,
-      removeAttributeQuotes: false,
-      sortClassName: false,
-      removeTagWhitespace: false,
-    });
 
-    const parts = minified.split(placeholderRx);
-    if (parts.length > 1) {
-      parts.forEach((value, i) => {
-        template
-          .get('quasi')
-          .get('quasis')
-          [i].replaceWith(
-            t.templateElement(
-              { cooked: value, raw: value },
-              i === parts.length - 1,
-            ),
+    return {
+      visitor: {
+        TaggedTemplateExpression(path) {
+          const tag = path.get('tag');
+          const isHtmlTag = getNames('tags', this.opts.tags).some(name =>
+            tag.isIdentifier({ name }),
           );
-      });
-      template.replaceWith(template.get('quasi'));
-    } else {
-      template.replaceWithSourceString(`'${escapeStaticString(minified)}'`);
-    }
-  }
-
-  return {
-    visitor: {
-      TaggedTemplateExpression(path) {
-        const tag = path.get('tag');
-        const isHtmlTag = getNames('tags', this.opts.tags).some(name =>
-          tag.isIdentifier({ name }),
-        );
-        if (isHtmlTag) {
-          minify(path, this.opts);
-        }
+          if (isHtmlTag) {
+            minify(path, this.opts);
+          }
+        },
       },
-    },
+    };
   };
-};
